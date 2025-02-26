2079a0f374da665488bf05c29d61d0b46b206349
Sam Jeong <sam@banf.ai>
2025-02-26 16:26:25 +0900

    Minor  change

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react'
 import io from 'socket.io-client'
 
-const socket = io('http://10.42.0.173:4000')
+const socket = io('http://54.180.43.6:4000')
 
 export function Navbar() {
   const [isStarted, setIsStarted] = useState(false)
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import io from 'socket.io-client'
 
-const socket = io('http://10.42.0.173:4000')
+const socket = io('http://54.180.43.6:4000')
 
 export default function Page() {
   const [selectedLoad, setSelectedLoad] = useState(`500`)
